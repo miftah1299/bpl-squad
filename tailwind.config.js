07c8be17945906549e9e3d860e49f6e69f944eef
Your Name <jannat27120@gmail.com>
2024-11-01 13:54:35 +0600

    Enhance Header component, and configure Tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: '##E7FE29',
+            },
+            fontFamily: {
+                sora: ['"Sora"', 'sans-serif'], 
+            },
+            backgroundImage: {
+                'custom-pattern': "url('/src/assets/banner-main.png')",
+            },
+        },
     },
     plugins: [],
 };
